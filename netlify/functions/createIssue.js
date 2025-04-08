12494dfd0669e7b015eb0e31370ed7600b75a828
somerandomscripts <joppynoo@gmail.com>
2025-04-08 11:40:06 -0500

    Update createIssue.js

--- netlify/functions/createIssue.js
+++ netlify/functions/createIssue.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 exports.handler = async function(event) {
 	const { data, user, token, captcha } = JSON.parse(event.body);
